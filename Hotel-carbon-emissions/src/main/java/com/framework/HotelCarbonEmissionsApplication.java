4abcb44b45cae6e91a6091de9adbeef125711927
xiaoxianchen <1318220327@qq.com>
2022-12-11 20:51:47 +0800

    version01

---
+++ Hotel-carbon-emissions/src/main/java/com/framework/HotelCarbonEmissionsApplication.java
@@ -0,0 +1,13 @@
+package com.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCarbonEmissionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelCarbonEmissionsApplication.class, args);
+    }
+
+}
